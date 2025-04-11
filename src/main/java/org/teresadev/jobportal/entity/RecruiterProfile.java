7568d98be1ae05b871c1d9cf135df6ea3216d8c0
Teresa <teresagalaviza@gmail.com>
2025-04-11 11:23:11 -0600

    file directory properties2

--- src/main/java/org/teresadev/jobportal/entity/RecruiterProfile.java
+++ src/main/java/org/teresadev/jobportal/entity/RecruiterProfile.java
@@ -117,7 +117,7 @@ public class RecruiterProfile {
     @Transient
     public String getPhotosImagePath() {
         if (profilePhoto == null) return null;
-        return "/photos/recruiter/" + userAccountId + "/" + profilePhoto;
+        return "/tmp/photos/recruiter/" + userAccountId + "/" + profilePhoto;
     }
 
     @Override
